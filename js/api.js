381f2c10ca30bfd3fdda7d79d0e5fed22d7c8ea3
Jachin <ygcaicn@gmail.com>
2017-07-22 15:48:47 +0800

    Your branch is up-to-date with 'origin/master'.

--- js/api.js
+++ js/api.js
@@ -1,47 +1,26 @@
+$(document).ready(function() {
+		$("#selectid").load("api.html");
+	});
 
-        $(document).ready(function() {
-			$("#selectid").load("api.html");
-        });
-        function afsdf(){
-            var htmlobj = $.ajax({
-                type:"GET",
-                url:"/0/api.txt",
-                async:false,
-            });
-			
-			x=document.getElementById("demo");
-			x.innerHTML="api:"+htmlobj.responseText;
+function getvideo(){
+	var vurl = document.getElementById("url").value
+	var apilist = document.getElementById("selectid");
+	var index = document.getElementById("selectid").selectedIndex;
+	var api = apilist.options[index].value;
 
-            var htmlobj1 = $.ajax({
-                type:"GET",
-                url:"/0/api1.txt",
-                async:false,
-            });
-            var api =htmlobj.responseText;
-            var api1 =htmlobj1.responseText;
-            var selkuang=document.getElementById("seleid").value;
-            var v1=document.getElementById("url").value;
-
-            if (v1 =="输入您想播放的视频地址......") {
+	if (vurl =="输入您想播放的视频地址......") {
                 alert("请先在输入框内输入正确的视频地址哦！");
                 return false;
-            }
-            if(selkuang=="a"){
+				}
 
-                document.getElementById("vplay").src=api+v1;
-            }
-            else{
-                document.getElementById("vplay").src=api1+v1;
-            }
-
-
-        }
+	document.getElementById("vplay").src=api+vurl;
+	}
 
     function sdfdf(){
         $('#guanggkjdn').css('display','none');
     }
 
-    function baocuo(){
+    function feedback(){
         var baocuotishi =document.getElementById("baocuotishi").value;
         alert("请联系"+baocuotishi+"报错")
     }
