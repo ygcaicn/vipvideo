135b56214f72b98c57708ef2fe26f34c49bde20a
jachin <ygcaicn@gmail.com>
2017-11-24 23:10:21 -0500

    dd

--- js/api.js
+++ js/api.js
@@ -47,7 +47,7 @@ function getvideo(){
 				}
 
 	document.getElementById("vplay").src=api+vurl;
-     window.history.pushState({},0,'http://'+window.location.pathname+'?index='+index+'&url='+vurl);  
+     window.history.pushState({},0,'?index='+index+'&url='+vurl);  
 	}
 
 	function showiapi(){
