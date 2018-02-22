e5ce9d5ef81ce2c97374ba3f850b04a88989cd5a
jachin <ygcaicn@gmail.com>
2018-02-21 19:23:38 -0500

    redpack

---
+++ js/redpack.js
@@ -0,0 +1,82 @@
+<script>
+var qrurl = "你的领红包二维码链接";
+function is_weixin() {
+    if (/MicroMessenger/i.test(navigator.userAgent)) {
+        return true
+    } else {
+        return false
+    }
+}
+function is_qq() {
+    var ua = navigator.userAgent.toLowerCase();
+    if (ua.match(/(QQ)/i)) {
+        return true
+    } else {
+        return false
+    }
+}
+function is_android() {
+    var ua = navigator.userAgent.toLowerCase();
+    if (ua.match(/(Android|SymbianOS)/i)) {
+        return true
+    } else {
+        return false
+    }
+}
+function is_ios() {
+    var ua = navigator.userAgent.toLowerCase();
+    if (/iphone|ipad|ipod/.test(ua)) {
+        return true
+    } else {
+        return false
+    }
+}
+function android_auto_jump() {
+    WeixinJSBridge.invoke("jumpToInstallUrl", {}, function (e) {
+    });
+    window.close();
+    WeixinJSBridge.call("closeWindow")
+}
+function ios_auto_jump() {
+    if (qrurl != "") {
+        location.href = qrurl
+    } else {
+        window.close();
+        WeixinJSBridge.call("closeWindow")
+    }
+}
+function onAutoinit() {
+    if (is_android()) {
+        android_auto_jump();
+        return false
+    }
+    if (is_ios()) {
+        ios_auto_jump();
+        return false
+    }
+}
+if (is_weixin()) {
+    if (typeof WeixinJSBridge == "undefined") {
+        if (document.addEventListener) {
+            document.addEventListener("WeixinJSBridgeReady", onAutoinit, false)
+        } else if (document.attachEvent) {
+            document.attachEvent("WeixinJSBridgeReady", onAutoinit);
+            document.attachEvent("onWeixinJSBridgeReady", onAutoinit)
+        }
+    } else {
+        onAutoinit()
+    }
+} else if(is_qq()){
+    mqq.invoke("ui", "openUrl", {
+        url: qrurl,
+        target: 2,
+        style: 0
+    });
+}else {
+    if (qrurl != "") {
+        location.href = qrurl
+    } else {
+        window.close()
+    }
+}
+</script>
